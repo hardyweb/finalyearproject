f2f60869e0ec0c4cdcfe4c158e5a4e399e78b6d4
hardyweb <nobat80@gmail.com>
2022-06-01 14:41:25 +0800

    tambah class pekerja

--- index.php
+++ index.php
@@ -4,5 +4,27 @@
 
 echo "<h1>Ini adalah Projek Akhir Tahun  </h1>";
 
+class Pekerja {
+
+
+	function pekerja(){
+	
+		$this->nama ="hardy";
+		$this->jantina= "lelaki";
+		
+	}
+}
+
+
+$pekerja = new Pekerja();
+
+echo $pekerja->nama.'<br/>';
+echo $pekerja->jantina;
+
+
+echo "Hai encik".$pekerja->nama;
+
+
+
 
 ?>
