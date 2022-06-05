883934e879e58a2b35f8813af57d0fdfa56de197
hardyweb <nobat80@gmail.com>
2022-06-05 09:41:18 +0800

    asingkan header.php, footer.php, content.php, class.php

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
 
+include('header.php');
+
 
 
 echo "<h1>Ini adalah Projek Akhir Tahun  </h1>";
 
-class Pekerja {
+
+include('content.php');
 
 
-	function pekerja(){
-	
-		$this->nama ="hardy";
-		$this->jantina= "lelaki";
-		
-	}
-}
-
-
-$pekerja = new Pekerja();
-
-echo $pekerja->nama.'<br/>';
-echo $pekerja->jantina;
-
-
-echo "Hai encik".$pekerja->nama;
-
-
+include('footer.php');
 
 
 ?>
